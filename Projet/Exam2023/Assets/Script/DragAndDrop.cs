0ed64619cc846d7d1b3b8938f74a923ba0115920
Hugo SIMON <h.simon@ludus-academie.com>
2023-06-23 11:54:04 +0200

    Glisse object in Content
    On peut déplacer les carrés rouges, et les remettre dans le Content

---
+++ Projet/Exam2023/Assets/Script/DragAndDrop.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private bool firstTime = true;
+    [SerializeField] private GameObject Content;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (firstTime)
+        {
+            firstTime = false;
+            MoveTwoParentsUp();
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        RectTransform contentRect = Content.GetComponent<RectTransform>();
+        if (RectTransformUtility.RectangleContainsScreenPoint(contentRect, eventData.position))
+        {
+            transform.SetParent(Content.transform);
+        }
+    }
+
+    public void MoveTwoParentsUp()
+    {
+        Transform parent = transform.parent;
+        if (parent != null && parent.parent != null && parent.parent.parent != null)
+        {
+            transform.SetParent(parent.parent.parent);
+        }
+    }
+}
